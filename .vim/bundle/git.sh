138065699682e3f25f26a2c7382338d2858fade8
samling <samlingx@gmail.com>
2013-08-26 19:22:36 -0700

    Auto git clone script for vundle bundles

---
+++ .vim/bundle/git.sh
@@ -0,0 +1,5 @@
+echo "Cloning to "`pwd` 
+
+git clone https://github.com/gmarik/vundle.git
+git clone https://github.com/Lokaltog/vim-easymotion.git
+git clone https://github.com/scrooloose/nerdtree.git
