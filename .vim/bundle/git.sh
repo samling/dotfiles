2682e84bf43662da2429f05f2558c58976c563ce
samling <samlingx@gmail.com>
2013-08-30 09:06:08 -0700

    Added airline, patched fonts to install script

--- .vim/bundle/git.sh
+++ .vim/bundle/git.sh
@@ -1,6 +1,22 @@
 echo "Cloning to "`pwd` 
 
+echo "Cloning Vundle"
+echo ""
+echo ""
 git clone https://github.com/gmarik/vundle.git
+echo "Cloning easymotion"
+echo ""
+echo ""
 git clone https://github.com/Lokaltog/vim-easymotion.git
+echo "Cloning nerdtree"
+echo ""
+echo ""
 git clone https://github.com/scrooloose/nerdtree.git
+echo "Cloning airline"
+echo ""
+echo ""
 git clone https://github.com/bling/vim-airline.git
+echo "Cloning powerline fonts"
+echo ""
+echo ""
+git clone https://github.com/Lokaltog/powerline-fonts.git
