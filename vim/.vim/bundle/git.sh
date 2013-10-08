0e763ba133a1f9f3442072fa2c9129ff8c1581dd
samling <samlingx@gmail.com>
2013-10-08 01:07:23 -0700

    Removed a bunch of vim plugins

--- vim/.vim/bundle/git.sh
+++ vim/.vim/bundle/git.sh
@@ -2,17 +2,17 @@ echo "Cloning to "`pwd`
 echo ""
 git clone https://github.com/gmarik/vundle.git
 echo ""
-git clone https://github.com/tpope/vim-abolish.git
+#git clone https://github.com/tpope/vim-abolish.git
 echo ""
 git clone https://github.com/Lokaltog/vim-easymotion.git
 echo ""
-git clone https://github.com/scrooloose/nerdtree.git
+#git clone https://github.com/scrooloose/nerdtree.git
 echo ""
-git clone git://github.com/tpope/vim-surround.git
+#git clone git://github.com/tpope/vim-surround.git
 echo ""
-git clone http://github.com/sjl/gundo.vim.git
+#git clone http://github.com/sjl/gundo.vim.git
 echo ""
-git clone git://github.com/tpope/vim-commentary.git
+#git clone git://github.com/tpope/vim-commentary.git
 echo ""
 #git clone https://github.com/Lokaltog/powerline.git
 echo ""
@@ -20,11 +20,11 @@ git clone https://github.com/bling/vim-airline.git
 echo ""
 git clone https://github.com/Lokaltog/powerline-fonts.git
 echo ""
-git clone https://github.com/scrooloose/syntastic.git
+#git clone https://github.com/scrooloose/syntastic.git
 echo ""
-git clone https://github.com/kien/ctrlp.vim.git
+#git clone https://github.com/kien/ctrlp.vim.git
 echo ""
-git clone https://github.com/Valloric/MatchTagAlways.git
+#git clone https://github.com/Valloric/MatchTagAlways.git
 echo ""
 git clone https://github.com/godlygeek/csapprox.git
 echo ""
@@ -32,5 +32,5 @@ git clone https://github.com/vim-scripts/Better-CSS-Syntax-for-Vim.git
 echo ""
 git clone https://github.com/matze/vim-move.git
 echo ""
-git clone http://github.com/goldfeld/vim-seek.git
+#git clone http://github.com/goldfeld/vim-seek.git
 echo ""
