d093cefd9a42f6a6587280330b0b01d99132368f
samling <samlingx@gmail.com>
2017-01-25 19:55:21 -0800

    Removing legacy docker environment variables

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -16,6 +16,6 @@ export TMUX_POWERLINE_SEG_WEATHER_LOCATION="2411898"
 export ARCHFLAGS="-arch x86_64"
 
 # Docker
-export DOCKER_HOST=tcp://192.168.59.103:2376
-export DOCKER_CERT_PATH=/Users/sboynton/.boot2docker/certs/boot2docker-vm
-export DOCKER_TLS_VERIFY=1
+#export DOCKER_HOST=tcp://192.168.59.103:2376
+#export DOCKER_CERT_PATH=/Users/sboynton/.boot2docker/certs/boot2docker-vm
+#export DOCKER_TLS_VERIFY=1
