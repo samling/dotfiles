09defb12d3a26580ffd2ec5dff3d85aecd680922
samling <samlingx@gmail.com>
2018-03-06 21:50:55 -0800

    Simplified function name

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -1,3 +1,3 @@
-function git-status-all {
+function git-status {
     find . -type d -name '.git' | while read dir ; do sh -c "cd $dir/../ && git status -s | grep -q [azAZ09] && echo '\n\033[1m [ ${dir//\.git/} ]\n\033[m' && git status -s" ; done
 }
