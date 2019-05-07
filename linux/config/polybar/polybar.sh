ea278589b457cf0e9cff02806d641cd8f897930a
Sam Boynton <samlingx@gmail.com>
2019-05-06 17:33:39 -0700

    Adding more config files from current arch install

--- linux/config/polybar/polybar.sh
+++ linux/config/polybar/polybar.sh
@@ -1,8 +1,1 @@
-#!/usr/bin/env sh
-
-killall -q polybar
-
-while pgrep -x polybar >/dev/null; do sleep 1; done
-
-polybar -q main &
-polybar -q bottom &
+/home/sboynton/dotfiles/linux/config/polybar/polybar.sh
