1319c114cde3cfbee3df2e3d931d119d5fa8bc27
Sam Boynton <samlingx@gmail.com>
2022-08-23 23:34:44 -0700

    Add pip installs to PATH

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,8 +1,15 @@
 # Reset PATH
+#
 unset PATH
 
-# Using Homebrew without Linux CoreUtils
+# Essential paths
+#
 PATH=$PATH:/usr/local:/usr/local/bin:/usr/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin
 
+# pip
+#
+PATH=$PATH:${HOME}/.local/bin
+
 # Final path export
+#
 export PATH
