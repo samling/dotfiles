182a5cc71f9d489f8eea4193d6bc926f321b9921
Samuel Boynton <sboynton@nvidia.com>
2024-01-23 11:16:31 -0800

    Add a git-recent function

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -2,4 +2,8 @@ function git-status {
     find . -type d -name '.git' | while read dir ; do sh -c "cd $dir/../ && git status -s | grep -q [azAZ09] && echo '\n\033[1m [ ${dir//\.git/} ]\n\033[m' && git status -s" ; done
 }
 
-
+function git-recent {
+  RECENT_BRANCHES=$(for i in {1..10}; do echo -n "$i. "; git rev-parse --symbolic-full-name @{-$i} 2> /dev/null; done | fzf)
+  PREV_BRANCH=$(echo $RECENT_BRANCHES | cut -d'.' -f2 | sed 's/refs\/heads\///g' | tr -d ' ')
+  git checkout $PREV_BRANCH
+}
