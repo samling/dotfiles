e69f6365f861bc20075e5653a46db0b86b0e33b8
Samuel Boynton <sboynton@nvidia.com>
2024-02-29 09:57:02 -0800

    Remove lua extras

--- nvim/lua/custom/plugins/dashboard-nvim.lua
+++ nvim/lua/custom/plugins/dashboard-nvim.lua
@@ -20,7 +20,6 @@ return {
           { action = 'Telescope live_grep', desc = ' Find text', icon = ' ', key = 'g' },
           { action = [[lua require("lazyvim.util").telescope.config_files()()]], desc = ' Config', icon = ' ', key = 'c' },
           { action = 'lua require("persistence").load()', desc = ' Restore Session', icon = ' ', key = 's' },
-          { action = 'LazyExtras', desc = ' Lazy Extras', icon = ' ', key = 'x' },
           { action = 'Lazy', desc = ' Lazy', icon = '󰒲 ', key = 'l' },
           { action = 'qa', desc = ' Quit', icon = ' ', key = 'q' },
         },
