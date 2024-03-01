2e4e7280c7d4763683dc81889e0d1e931cc23640
Samuel Boynton <sboynton@nvidia.com>
2024-03-01 09:39:42 -0800

    Break out plugins into their own files, add/modify some keybindings

--- nvim/lua/custom/plugins/telescope-file-browser.lua
+++ nvim/lua/custom/plugins/telescope-file-browser.lua
@@ -2,6 +2,21 @@ return {
   'nvim-telescope/telescope-file-browser.nvim',
   dependencies = { 'nvim-telescope/telescope.nvim', 'nvim-lua/plenary.nvim' },
   config = function()
+    require('telescope').setup {
+      extensions = {
+        hijack_netrw = true,
+        file_browser = {
+          mappings = {
+            ['i'] = {
+              ['<C-d>'] = false,
+            },
+            ['n'] = {
+              d = false,
+            },
+          },
+        },
+      },
+    }
     require('telescope').load_extension 'file_browser'
   end,
 }
