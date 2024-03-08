45e37d49a5b09580ca9cb104a3ba22a61a571548
Samuel Boynton <sboynton@nvidia.com>
2024-03-07 16:46:49 -0800

    Don't lazyload hop; add new keybinds

--- nvim/lua/custom/plugins/hop.lua
+++ nvim/lua/custom/plugins/hop.lua
@@ -2,6 +2,7 @@ return {
   'smoka7/hop.nvim',
   version = '*',
   opts = {},
+  lazy = false,
   keys = function()
     local hop = require 'hop'
     local directions = require('hop.hint').HintDirection
@@ -16,6 +17,18 @@ return {
     end, { remap = true })
     vim.keymap.set('', 'T', function()
       hop.hint_char1 { direction = directions.BEFORE_CURSOR, current_line_only = false, hint_offset = 1 }
+    end, { remap = true })
+    vim.keymap.set('', '<leader>hl', function()
+      hop.hint_lines {}
+    end, { remap = true })
+    vim.keymap.set('', '<leader>hs', function()
+      hop.hint_lines_skip_whitespace {}
+    end, { remap = true })
+    vim.keymap.set('', '<leader>hw', function()
+      hop.hint_words {}
+    end, { remap = true })
+    vim.keymap.set('', '<leader>hp', function()
+      hop.hint_patterns {}
     end, { remap = true })
   end,
 }
