a967a7d63d891fa984c6e7a29c60ff7f66a948a0
Samuel Boynton <sboynton@nvidia.com>
2024-03-08 22:01:01 -0800

    Use catppuccin color scheme in nvim

--- nvim/lua/custom/plugins/colorscheme.lua
+++ nvim/lua/custom/plugins/colorscheme.lua
@@ -3,22 +3,34 @@ return { -- You can easily change to a different colorscheme.
   -- change the command in the config to whatever the name of that colorscheme is
   --
   -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`
-  'folke/tokyonight.nvim',
-  lazy = false, -- make sure we load this during startup if it is your main colorscheme
-  priority = 1000, -- make sure to load this before all the other start plugins
-  opts = {
-    transparent = true,
-    style = 'moon',
-    styles = {
-      sidebars = 'dark',
-      floats = 'dark',
-    },
-  },
+  --
+  'catppuccin/nvim',
+  name = 'catppuccin',
+  lazy = false,
+  priority = 1000,
   config = function()
+    require('catppuccin').setup {
+      flavour = 'mocha',
+      transparent_background = true,
+    }
     -- Load the colorscheme here
-    vim.cmd.colorscheme 'tokyonight-night'
+    vim.cmd.colorscheme 'catppuccin'
 
     -- You can configure highlights by doing something like
     vim.cmd.hi 'Comment gui=none'
   end,
 }
+
+-- Old themes
+--
+-- 'folke/tokyonight.nvim',
+-- lazy = false, -- make sure we load this during startup if it is your main colorscheme
+-- priority = 1000, -- make sure to load this before all the other start plugins
+-- opts = {
+--   transparent = true,
+--   style = 'moon',
+--   styles = {
+--     sidebars = 'dark',
+--     floats = 'dark',
+--   },
+-- },
