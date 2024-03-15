54ca973b4024aa842b7b110fbc49bfe590e8fb9b
Samuel Boynton <sboynton@nvidia.com>
2024-03-15 09:15:55 -0700

    Move some plugins to disabled folder for cleanliness

--- nvim/lua/custom/plugins/nvim-ts-context-commentstring.lua
+++ nvim/lua/custom/plugins/nvim-ts-context-commentstring.lua
@@ -1,1 +1,2 @@
+vim.g.skip_ts_context_commentstring_module = true
 return { 'JoosepAlviste/nvim-ts-context-commentstring' }
--- nvim/lua/custom/plugins/undotree.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'mbbill/undotree',
-  keys = {
-    { '<leader>u', mode = 'n', vim.cmd.UndotreeToggle, desc = 'Toggle undotree' },
-  },
-  config = function()
-    vim.g.undotree_WindowLayout = 2
-  end,
-}
