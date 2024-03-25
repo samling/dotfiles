f1158c816fad3f7c04c53b6ed69149edead36eff
Samuel Boynton <sboynton@nvidia.com>
2024-03-25 10:12:08 -0700

    Make ssh cell bold in tmux statusline

--- tmux/.tmux/scripts/ssh_hostname.sh
+++ tmux/.tmux/scripts/ssh_hostname.sh
@@ -4,7 +4,7 @@ function ssh_status_cell() {
     user=$(whoami)
     hostname=$(cat /etc/hostname)
     if [[ -n $SSH_CONNECTION ]]; then
-        echo "#[fg=#cba6f7,bg=default]#[fg=black,bg=#cba6f7] $user"@"$hostname #[fg=#cba6f7,bg=default] "
+        echo "#[fg=#cba6f7,bg=default]#[fg=black,bold,bg=#cba6f7] $user"@"$hostname #[fg=#cba6f7,bg=default] "
     fi
 }
 
