65223d43dc79717ff8f15f2d80a14d93ea8f6889
Sam Boynton <samlingx@gmail.com>
2024-06-04 09:19:09 -0700

    Change QMK plugin keymap to work for zodiarkpi; add other keymaps as comments

--- nvim/lua/custom/plugins/qmk.lua
+++ nvim/lua/custom/plugins/qmk.lua
@@ -9,9 +9,23 @@ return {
         'x x x x x x _ _ _ _ x x x x x x',
         'x x x x x x _ _ _ _ x x x x x x',
         'x x x x x x x x x x x x x x x x',
-        '_ _ _ x x x x x x x x x x _ _ _',
+        'x x x x x x x x x x x x x x x x',
+        'x x x x x _ x x x x _ x x x x x',
       },
     }
     require('qmk').setup(conf)
   end,
 }
+
+-- ZodiarkPi
+--   'x x x x x x _ _ _ _ x x x x x x',
+--   'x x x x x x _ _ _ _ x x x x x x',
+--   'x x x x x x x x x x x x x x x x',
+--   'x x x x x x x x x x x x x x x x',
+--   'x x x x x _ x x x x _ x x x x x',
+--
+-- Kyria
+--   'x x x x x x _ _ _ _ x x x x x x',
+--   'x x x x x x _ _ _ _ x x x x x x',
+--   'x x x x x x x x x x x x x x x x',
+--   '_ _ _ x x x x x x x x x x _ _ _',
