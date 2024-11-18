7f7552a3c83a0b80ffe78a610cd53b610126f6cf
Sam Boynton <samlingx@gmail.com>
2024-11-18 12:50:36 -0800

    Delete a ton of old linux configs that are no longer needed

--- linux/old/polybar/toggle.sh
+++
@@ -1,9 +0,0 @@
-bars=$(xdo id -N 'Polybar')
-bar=$(echo -e $bars | cut -d' ' -f1)
-state=$(xprop -id $bar)
-
-if echo $state | /usr/bin/grep "Normal"; then
-    xdo hide -N 'Polybar'
-else
-    xdo show -N 'Polybar'
-fi
