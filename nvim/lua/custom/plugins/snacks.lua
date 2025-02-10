85206432ff5435f212ca15dbbb49079d4a6f2eca
Sam Boynton <samlingx@gmail.com>
2025-02-10 15:40:32 -0800

    Add snacks, replace a couple of plugins

---
+++ nvim/lua/custom/plugins/snacks.lua
@@ -0,0 +1,22 @@
+return {
+  'folke/snacks.nvim',
+  priority = 1000,
+  lazy = false,
+  opts = {
+    animate = { enabled = true },
+    bigfile = { enabled = true },
+    dashboard = { enabled = true },
+    debug = { enabled = true },
+    git = { enabled = true },
+    indent = { enabled = true },
+    input = { enabled = true },
+    layout = { enabled = true },
+    notifier = { enabled = true },
+    notify = { enabled = true },
+    profiler = { enabled = true },
+    scroll = { enabled = true },
+    statuscolumn = { enabled = true },
+    toggle = { enabled = true },
+    util = { enabled = true },
+  }
+}
