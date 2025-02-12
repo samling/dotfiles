591341aaea042de2d82b90b27fb2a94772a4ddfd
Sam Boynton <samlingx@gmail.com>
2025-02-11 23:30:00 -0800

    Add zsh autosuggestions plugin

---
+++ zsh/plugins.zsh
@@ -0,0 +1,3 @@
+source ~/dotfiles/zsh/plugins/zsh-history-substring-search.zsh
+source ~/dotfiles/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+source ~/dotfiles/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
