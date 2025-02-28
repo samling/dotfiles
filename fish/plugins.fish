ff52a913107e1117b88c2b7234ee79d69bb5635a
Sam Boynton <samlingx@gmail.com>
2025-02-27 20:58:33 -0800

    Set fish as the interactive shell in ghostty if it exists; otherwise fall back to zsh

--- fish/plugins.fish
+++ fish/plugins.fish
@@ -1,7 +1,7 @@
 # Install Fisher if not present
-if not functions -q fisher
-    curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
-end
+#if not functions -q fisher
+#    curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
+#end
 
 # Essential plugins (install with Fisher)
 # fisher install PatrickF1/fzf.fish
