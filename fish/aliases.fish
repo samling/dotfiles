298de7f5cf1edff76fa3a5e50fcd3512a1e15c16
Sam Boynton <samlingx@gmail.com>
2025-02-27 22:03:48 -0800

    Replace aliases with functions; maybe fix the fisher installation; add fd to arch install

--- fish/aliases.fish
+++ fish/aliases.fish
@@ -1,33 +1,89 @@
 # Builtin Replacements
-alias cat "bat -pp"
-alias df "duf -hide special,fuse"
-alias grep "rg -u"
-alias top "btop"
-alias watch "viddy -d"
-alias vi "nvim-maybe-profile"
-alias vim "nvim-maybe-profile"
+function cat
+    bat -pp $argv
+end
 
-# ls aliases
-alias ls "lsd"
-alias l "ls -a"
-alias ll "ls -lahg"
-alias lll "ls -lahg --blocks permission,user,group,size,date,name"
+function df
+    duf -hide special,fuse $argv
+end
 
-# Kubernetes aliases
-alias k "kubectl"
-alias kubectx "FZF_DEFAULT_OPTS='--reverse' kubectl ctx"
-alias kx "FZF_DEFAULT_OPTS='--reverse' kubectl ctx"
-alias kubens "FZF_DEFAULT_OPTS='--reverse' kubectl-ns"
-alias kn "FZF_DEFAULT_OPTS='--reverse' kubectl-ns"
+function grep
+    rg -u $argv
+end
+
+function top
+    btop $argv
+end
+
+function watch
+    viddy -d $argv
+end
+
+function vi
+    nvim-maybe-profile $argv
+end
+
+function vim
+    nvim-maybe-profile $argv
+end
+
+# ls functions
+function ls
+    lsd $argv
+end
+
+function l
+    ls -a $argv
+end
+
+function ll
+    ls -lahg $argv
+end
+
+function lll
+    ls -lahg --blocks permission,user,group,size,date,name $argv
+end
+
+# Kubernetes functions
+function k
+    kubectl $argv
+end
+
+function kubectx
+    env FZF_DEFAULT_OPTS='--reverse' kubectl ctx $argv
+end
+
+function kx
+    env FZF_DEFAULT_OPTS='--reverse' kubectl ctx $argv
+end
+
+function kubens
+    env FZF_DEFAULT_OPTS='--reverse' kubectl-ns $argv
+end
+
+function kn
+    env FZF_DEFAULT_OPTS='--reverse' kubectl-ns $argv
+end
 
 # AI tools
-alias ai "aichat"
-alias gpt "aichat -m openai:gpt-4-0125-preview"
-alias claude "aichat -m claude:claude-3-sonnet-20240229"
+function ai
+    aichat $argv
+end
 
-# Other aliases
-alias lg "lazygit"
-alias yless "jless --yaml"
-#alias R "source ~/.config/fish/config.fish"
-alias R "exec $SHELL -l" # this seems to mitigate issues with various invocations being stacked multiple times from sourcing the config again
-alias tmux "direnv exec / tmux" 
+# Other functions
+function lg
+    lazygit $argv
+end
+
+function yless
+    jless --yaml $argv
+end
+
+function R
+    # reload fish without re-sourcing everything
+    exec $SHELL -l
+end
+
+function tmux
+    direnv exec / tmux $argv
+end
--- fish/plugins.fish
+++ fish/plugins.fish
@@ -1,9 +1,11 @@
 # Install Fisher if not present
-#if not functions -q fisher
-#    curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
-#end
+if not functions -q fisher
+    curl -sL https://git.io/fisher | source
+    fisher install jorgebucaran/fisher
+end
 
 # Essential plugins (install with Fisher)
+fisher install pure-fish/pure
 # fisher install PatrickF1/fzf.fish
 # fisher install IlanCosman/tide@v5
 # fisher install franciscolourenco/done
