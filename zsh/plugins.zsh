cd43de42acecae0635c175bb09679ca7e25c7b9b
Sam Boynton <samlingx@gmail.com>
2025-02-28 08:44:47 -0800

    Add Windows alacritty configuration; add fzf-tab plugin

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -1,3 +1,4 @@
+# source ~/dotfiles/zsh/plugins/fzf-tab/fzf-tab.plugin.zsh
 source ~/dotfiles/zsh/plugins/zsh-history-substring-search.zsh
 source ~/dotfiles/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 source ~/dotfiles/zsh/plugins/zsh-autosuggestions.zsh
