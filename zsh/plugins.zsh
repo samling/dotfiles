e455f2275267849494648eaf5b6534b93700e34a
Sam Boynton <samlingx@gmail.com>
2025-03-01 08:36:41 -0800

    Not sure what's going on with plugin import order

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -1,4 +1,4 @@
 # Note: zsh-syntax-highlighting *must* be first, or at least before autosuggestions and zsh-history-substring-search, otherwise there's some funky recursion going on
-source ~/dotfiles/zsh/plugins/zsh-history-substring-search.zsh
 source ~/dotfiles/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+source ~/dotfiles/zsh/plugins/zsh-history-substring-search.zsh
 source ~/dotfiles/zsh/plugins/zsh-autosuggestions.zsh
