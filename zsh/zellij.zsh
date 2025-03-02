65cfcaa10e5489bd70d8e95031724cf07b6c12c3
Sam Boynton <samlingx@gmail.com>
2025-03-02 00:11:30 -0800

    Emulate tmux default tab naming as shell; fix wl-paste; theme some stuff

--- zsh/zellij.zsh
+++ zsh/zellij.zsh
@@ -1,28 +1,38 @@
-function current_dir() {
-    local current_dir=$PWD
-    if [[ $current_dir == $HOME ]]; then
-        current_dir="~"
-    else
-        current_dir=${current_dir##*/}
+# function current_dir() {
+#     local current_dir=$PWD
+#     if [[ $current_dir == $HOME ]]; then
+#         current_dir="~"
+#     else
+#         current_dir=${current_dir##*/}
+#     fi
+#
+#     echo $current_dir
+# }
+
+# function change_tab_title() {
+#     local title=$1
+#     command nohup zellij action rename-tab $title >/dev/null 2>&1
+# }
+#
+# function set_tab_to_working_dir() {
+#     local result=$?
+#     local title=$(current_dir)
+#     # uncomment the following to show the exit code after a failed command
+#     # if [[ $result -gt 0 ]]; then
+#     #     title="result]" 
+#     # fi
+#
+#     change_tab_title $title
+# }
+#
+# This function automatically renames the default "Tab #1", "Tab #2" etc. to "zsh".
+# Since it's highly unlikely we'll ever rename our tabs to the original zellij format,
+# this should hopefully not cause any conflicts.
+function rename_default_tab_name_to_current_shell() {
+    local shell=$(echo ${SHELL##*/}) # print only 'zsh' from e.g. '/usr/bin/zsh'
+    if $(zellij action query-tab-names | grep -q "Tab #"); then
+      command nohup zellij action rename-tab $shell >/dev/null 2>&1
     fi
-    
-    echo $current_dir
-}
-
-function change_tab_title() {
-    local title=$1
-    command nohup zellij action rename-tab $title >/dev/null 2>&1
-}
-
-function set_tab_to_working_dir() {
-    local result=$?
-    local title=$(current_dir)
-    # uncomment the following to show the exit code after a failed command
-    # if [[ $result -gt 0 ]]; then
-    #     title="result]" 
-    # fi
-
-    change_tab_title $title
 }
 
 function set_tab_to_command_line() {
@@ -31,6 +41,7 @@ function set_tab_to_command_line() {
 }
 
 if [[ -n $ZELLIJ ]]; then
+    add-zsh-hook precmd rename_default_tab_name_to_current_shell
     # add-zsh-hook precmd set_tab_to_working_dir
     # add-zsh-hook preexec set_tab_to_command_line
 fi
