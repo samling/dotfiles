2ff8af4b2c5e9e3bd13b38437ce0f22bd58393c7
Sam Boynton <samlingx@gmail.com>
2025-03-04 11:22:05 -0800

    Make option/alt-left|right work in wezterm

--- linux/config/wezterm/.wezterm.lua
+++ linux/config/wezterm/.wezterm.lua
@@ -11,4 +11,11 @@ config.font_size = 12
 
 config.color_scheme = "Catppuccin Mocha"
 
+config.keys = {
+  -- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
+  {key="LeftArrow", mods="OPT", action=wezterm.action{SendString="\x1bb"}},
+  -- Make Option-Right equivalent to Alt-f; forward-word
+  {key="RightArrow", mods="OPT", action=wezterm.action{SendString="\x1bf"}},
+}
+
 return config
--- macos/config/wezterm/.wezterm.lua
+++ macos/config/wezterm/.wezterm.lua
@@ -1,13 +1,23 @@
 local wezterm = require('wezterm')
 
-local config = {
-  enable_wayland = false,
-  animation_fps = 120,
-  enable_tab_bar = false,
-  font = wezterm.font('IosevkaTerm NFM'),
-  font_size = 14,
-  color_scheme = "Catppuccin Mocha",
-  window_decorations = "RESIZE"
+local config = wezterm.config_builder()
+
+config.enable_wayland = false
+config.animation_fps = 120
+config.enable_tab_bar = false
+
+config.font = wezterm.font('IosevkaTerm NFM')
+config.font_size = 14
+
+config.color_scheme = "Catppuccin Mocha"
+
+config.window_decorations = "RESIZE"
+
+config.keys = {
+  -- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
+  {key="LeftArrow", mods="OPT", action=wezterm.action{SendString="\x1bb"}},
+  -- Make Option-Right equivalent to Alt-f; forward-word
+  {key="RightArrow", mods="OPT", action=wezterm.action{SendString="\x1bf"}},
 }
 
 return config
