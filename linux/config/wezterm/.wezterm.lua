d1b1e4ab0726b80d0425ae6a7e5864be24f4cb5f
Sam Boynton <samlingx@gmail.com>
2025-03-08 15:52:41 -0800

    Don't use strictly monospaced font

--- linux/config/wezterm/.wezterm.lua
+++ linux/config/wezterm/.wezterm.lua
@@ -6,7 +6,7 @@ config.enable_wayland = false
 config.animation_fps = 120
 config.enable_tab_bar = false
 
-config.font = wezterm.font 'Iosevka Nerd Font Mono'
+config.font = wezterm.font 'Iosevka Nerd Font'
 config.font_size = 12
 
 config.color_scheme = "Catppuccin Mocha"
