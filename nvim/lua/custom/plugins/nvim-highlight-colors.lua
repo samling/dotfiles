118ab6bc1653c4066616caf2e93b94ee65a78bdf
Sam Boynton <samlingx@gmail.com>
2025-03-07 21:43:32 -0800

    Reenable nvim-highlight-colors

--- nvim/lua/custom/plugins/nvim-highlight-colors.lua
+++ nvim/lua/custom/plugins/nvim-highlight-colors.lua
@@ -2,7 +2,7 @@ return {
   'brenoprata10/nvim-highlight-colors',
   config = function()
     require('nvim-highlight-colors').setup {
-      render = 'virtual', -- foreground, background, virtual
+      render = 'background', -- foreground, background, virtual
     }
   end,
 }
