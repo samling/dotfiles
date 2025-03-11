75b25d7720e36486b277066be09be1343fa3d79e
Sam Boynton <samlingx@gmail.com>
2025-03-11 11:53:48 -0700

    Port wezterm config for mac

--- macos/config/wezterm/.wezterm.lua
+++ macos/config/wezterm/.wezterm.lua
@@ -13,11 +13,42 @@ config.color_scheme = "Catppuccin Mocha"
 
 config.window_decorations = "RESIZE"
 
+config.disable_default_key_bindings = true
 config.keys = {
   -- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
   {key="LeftArrow", mods="OPT", action=wezterm.action{SendString="\x1bb"}},
   -- Make Option-Right equivalent to Alt-f; forward-word
   {key="RightArrow", mods="OPT", action=wezterm.action{SendString="\x1bf"}},
+  {
+    key = "P",
+    mods = "CMD|SHIFT",
+    action = wezterm.action.ActivateCommandPalette
+  },
+  {
+    key = "P",
+    mods = "CTRL|SHIFT",
+    action = wezterm.action.ActivateCommandPalette
+  },
+  {
+    key = "C",
+    mods = "CMD|SHIFT",
+    action = wezterm.action.CopyTo('Clipboard')
+  },
+  {
+    key = "C",
+    mods = "CTRL|SHIFT",
+    action = wezterm.action.CopyTo('Clipboard')
+  },
+  {
+    key = "P",
+    mods = "CMD|SHIFT",
+    action = wezterm.action.PasteFrom('Clipboard')
+  },
+  {
+    key = "P",
+    mods = "CTRL|SHIFT",
+    action = wezterm.action.PasteFrom('Clipboard')
+  },
 }
 
 return config
