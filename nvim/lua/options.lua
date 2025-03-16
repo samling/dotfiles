f9166f222f07a27807a9b74a482c80e7611d9d62
Sam Boynton <samlingx@gmail.com>
2025-03-16 01:05:15 -0700

    nvim leader back to space

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -6,7 +6,7 @@
 -- Set <space> as the leader key
 -- See `:help mapleader`
 -- Must happen before plugins are loaded (otherwise wrong leader will be used)
-vim.g.mapleader = ','
+vim.g.mapleader = ' '
 -- vim.g.maplocalleader = ' '
 
 -- [[ Options ]]
