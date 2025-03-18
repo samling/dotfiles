459a27488447f57e6427c07f651282edfaabfcdd
Sam Boynton <samlingx@gmail.com>
2025-03-18 16:36:21 -0700

    Use flash.nvim instead of hop

--- nvim/lua/custom/plugins/flash.lua
+++ nvim/lua/custom/plugins/flash.lua
@@ -4,11 +4,23 @@ return {
   ---@type Flash.Config
   opts = {},
   -- stylua: ignore
-  keys = {
-    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-    -- { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-    -- { "r", mode = "o", function() require("flash").remote() end, desc = "remote flash" },
-    -- { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-    -- { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
-  },
+      keys = {
+        {
+          's',
+          function()
+            require('flash').jump()
+          end,
+          mode = { 'n', 'x', 'o' },
+          desc = 'Jump forwards',
+        },
+        {
+          'S',
+          function()
+            require('flash').jump({ search = { forward = false } })
+          end,
+          mode = { 'n', 'x', 'o' },
+          desc = 'Jump backwards',
+        },
+      },
 }
+
