b3f6aa989a71fa547a84852a8c7edb71a3c03049
Sam Boynton <samlingx@gmail.com>
2025-03-25 23:59:19 -0700

    Working popover example

--- dot_config/ags/widget/bar/modules/audio/index.tsx
+++ dot_config/ags/widget/bar/modules/audio/index.tsx
@@ -1,27 +1,28 @@
 import Wp from "gi://AstalWp"
-import { bind } from "astal"
+import { bind, Variable } from "astal"
 import { App, Gtk } from "astal/gtk3"
 import AudioWindow from "./helper/AudioWindow"
+import Popover from "./helper/AudioWindow"
 
 export default function AudioSlider() {
     const speaker = Wp.get_default()?.audio.defaultSpeaker!
+
+    const visible = Variable(false);
     
-    // Create the audio window when the component is initialized
-    const setup = (widget: Gtk.Widget) => {
-        return AudioWindow()
-    }
+    const _popover = <Popover
+        onClose={() => {
+            visible.set(false)
+        }}
+        visible={visible()}
+    >
+    </Popover>
     
     // Show just the icon button that triggers the floating window
     return <button
         className="AudioIcon"
         tooltipText="Audio Settings"
-        setup={setup}
-        onClick={() => {
-            const audioWindow = App.get_window("audio-control-window")
-            if (audioWindow) {
-                const isVisible = audioWindow.get_visible()
-                audioWindow.set_visible(!isVisible)
-            }
+        onClicked={() => {
+            visible.set(!visible.get())
         }}>
         <icon icon={bind(speaker, "volumeIcon")} />
     </button>
