67abdd9f05fce935674254ef3d2dd9077e8c34f4
Sam Boynton <samlingx@gmail.com>
2025-04-25 00:37:23 -0700

    Rotate the icon correctly when opening or closing the popup

--- dot_config/ags/widget/Bar/modules/ControlCenterButton.tsx
+++ dot_config/ags/widget/Bar/modules/ControlCenterButton.tsx
@@ -1,29 +1,32 @@
 import { App, Widget } from "astal/gtk3";
-import { bind } from "astal";
+import { bind, Variable } from "astal";
 import { toggleWindow } from "../../../utils";
 import { visible } from "../../ControlCenter/ControlCenter"
 
 
 export default function ControlCenterButton() {
-
-    const iconSetup = (icon: Widget.Icon) => {
-        App.connect("window-toggled", (_, window) => {
-            if (window.name === "controlcenter")
-                icon.css = window.get_visible() ? '-gtk-icon-transform: rotate(90deg);' : '-gtk-icon-transform: rotate(0deg);'
-        })
-    }
-
+    const rotated = Variable(false);
+    
+    const setupButton = (button: Widget.Button) => {
+        // Subscribe to visible changes to keep rotation in sync
+        visible.subscribe(isVisible => {
+            rotated.set(isVisible);
+        });
+    };
+    
     return (
         <button className="controlCenterButton"
         onClick={() => {
-            const currentValue = visible.get();
-            const newValue = !currentValue;
+            const newValue = !visible.get();
             visible.set(newValue);
+            toggleWindow("controlcenter");
         }}
+        setup={setupButton}
         >
             <icon icon="pan-end-symbolic"
             className="controlCenterIcon"
-            setup={iconSetup}
+            css={Variable.derive([rotated], (r: boolean) => 
+                r ? '-gtk-icon-transform: rotate(90deg);' : '-gtk-icon-transform: rotate(0deg);')()}
             />
         </button>
     )
