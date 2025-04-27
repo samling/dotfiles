e466a51be72414ee21d13f519d041f2122386ff9
Sam Boynton <samlingx@gmail.com>
2025-04-26 23:01:07 -0700

    Update jwt-decode function name

--- dot_zsh/functions.zsh
+++ dot_zsh/functions.zsh
@@ -49,7 +49,7 @@ fman() {
 }
 
 # decode JWTs
-jwtdecode() {
+jwt-decode() {
   if [[ -x $(command -v jq) ]]; then
     jq -R 'split(".") | .[0],.[1] | @base64d | fromjson' <<< "${1}"
     echo "Signature: $(echo "${1}" | awk -F'.' '{print $3}')"
