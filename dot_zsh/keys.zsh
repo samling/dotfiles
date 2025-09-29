ee466c6623d207f4709aef78ffc0a849e64b2ae4
Sam Boynton <samlingx@gmail.com>
2025-09-29 13:38:14 -0700

    Update widget invocation for cs; increase cs fzf height

--- dot_zsh/keys.zsh
+++ dot_zsh/keys.zsh
@@ -27,13 +27,27 @@ bindkey '^xe' edit-command-line
 bindkey '^x^e' edit-command-line
 
 #========= C-s to bring up the cs menu
+# Disable terminal flow control (Ctrl+S/Ctrl+Q)
+# This allows Ctrl+S to be used as a keybinding
+stty -ixon 2>/dev/null
 
-function cs-select() {
-  RBUFFER=$(cs exec)
-  CURSOR=$#BUFFER
-  zle redisplay
+# Command snippets widget - runs cs exec and captures stdout
+cs-exec-widget() {
+    # Run cs exec and capture stdout (TUI displays on stderr)
+    local cmd=$(cs exec)
+    
+    # Check if we got a command (not cancelled)
+    if [[ $? -eq 0 && -n "$cmd" ]]; then
+        # Insert at current position
+        LBUFFER="${LBUFFER}${cmd}"
+    fi
+    
+    # Redraw
+    zle redisplay
 }
-zle -N cs-select
-stty -ixon
-bindkey -r '^s'
-bindkey '^s' cs-select
+
+# Register the widget
+zle -N cs-exec-widget
+
+# Bind Ctrl+S to the widget
+bindkey '^S' cs-exec-widget
