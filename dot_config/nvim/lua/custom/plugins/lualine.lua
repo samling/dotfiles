5ffef8a45ddefc87d162d2650eaa5be38461a231
Sam Boynton <samlingx@gmail.com>
2026-03-15 10:56:16 -0700

    Reintroduce matugen piecemeal

--- dot_config/nvim/lua/custom/plugins/lualine.lua
+++ dot_config/nvim/lua/custom/plugins/lualine.lua
@@ -86,7 +86,7 @@ return {
     require('lualine').setup {
       options = {
         icons_enabled = true,
-        theme = 'auto',
+        theme = 'base16',
         -- component_separators = { left = "", right = "" },
         -- section_separators = { left = "", right = "" },
         component_separators = { left = '|', right = '|' },
