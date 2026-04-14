535f79c67624c09361c41129fe7aa7811d5e2d04
Sam Boynton <samlingx@gmail.com>
2026-04-14 10:00:38 -0700

    Enable home-manager and flakes for nix

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "NixOS home configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { nixpkgs, home-manager, ... }: {
+    homeConfigurations."sboynton" = home-manager.lib.homeManagerConfiguration {
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      modules = [ ./home.nix ];
+    };
+  };
+}
---
+++ home.nix
@@ -0,0 +1,11 @@
+{ pkgs, ... }: {
+  home.username = "sboynton";
+  home.homeDirectory = "/home/sboynton";
+  home.stateVersion = "24.11";
+
+  home.packages = with pkgs; [
+    # Add packages here
+  ];
+
+  programs.home-manager.enable = true;
+}
