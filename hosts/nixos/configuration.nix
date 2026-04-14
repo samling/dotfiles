180c3666786cf28fca6b684a903a2ad702090a14
samling <samlingx@gmail.com>
2026-04-14 16:11:25 -0700

    Enable openssh

--- hosts/nixos/configuration.nix
+++ hosts/nixos/configuration.nix
@@ -23,6 +23,8 @@
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
+  services.openssh.enable = true;
+
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
