e2f9ef048c712558c9f880dd9de12506ceab9d32
Sam Boynton <samlingx@gmail.com>
2026-04-14 10:34:54 -0700

    Define default user

--- shared/default.nix
+++ shared/default.nix
@@ -1,4 +1,11 @@
 { ... }: {
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  users.users.sboynton = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "networkmanager" ];
+    home = "/home/sboynton";
+  };
+
   programs.hyprland.enable = true;
 }
