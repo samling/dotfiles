77f7c518132fa2363d5d131c87e1b08d4048825f
Sam B <samlingx@gmail.com>
2026-05-12 23:15:21 -0700

    fish: simplify __ctrl_c_preserve (use kill-line from cursor 0)

--- chezmoi/dot_config/fish/functions/__ctrl_c_preserve.fish
+++ chezmoi/dot_config/fish/functions/__ctrl_c_preserve.fish
@@ -1,9 +1,14 @@
-function __ctrl_c_preserve --description 'cancel commandline but stash partial to the kill-ring (yankable with C-y)'
+function __ctrl_c_preserve --description 'cancel: stash partial cmd in kill-ring, recover with C-y'
     set -l buf (commandline)
-    if test -n "$buf"
-        # kill-whole-line drops current input into the kill-ring,
-        # then cancel-commandline prints the `^C` marker and gives a new prompt.
-        commandline -f kill-whole-line
+    if test -z "$buf"
+        # empty line: fall through to default cancel (prints ^C, new prompt)
+        commandline -f cancel-commandline
+        return
     end
+
+    # Move cursor to start of buffer, then kill to end-of-line.
+    # kill-line stores the killed text in the kill-ring (yank with C-y).
+    commandline -C 0
+    commandline -f kill-line
     commandline -f cancel-commandline
 end
