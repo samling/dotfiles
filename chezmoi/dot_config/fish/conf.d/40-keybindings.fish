dd98af012cd42438673e289a3fa001a67389d391
Sam B <samlingx@gmail.com>
2026-05-12 23:08:25 -0700

    fish: bind Ctrl-T to fzf.fish directory search (replaces transpose-chars)

--- chezmoi/dot_config/fish/conf.d/40-keybindings.fish
+++ chezmoi/dot_config/fish/conf.d/40-keybindings.fish
@@ -9,6 +9,13 @@ function fish_user_key_bindings
     # C-s: command-snippets widget
     bind \cs cs-exec-widget
 
+    # C-t: fzf file/directory search (replaces fish default `transpose-chars`).
+    # fzf.fish ships _fzf_search_directory and binds it to Alt-C-F by default;
+    # we override Ctrl-T here to match zsh muscle memory.
+    if functions -q _fzf_search_directory
+        bind \ct _fzf_search_directory
+    end
+
     # Free C-l, C-j for tmux; free C-p, C-n (we use UP/DOWN for history)
     bind --erase \cl 2>/dev/null
     bind --erase \cj 2>/dev/null
