dc6fa73665a95e0d3c562d99355f69961dd04045
Sam B <samlingx@gmail.com>
2026-05-15 22:59:32 -0700

    Add opencode

--- decman/modules/common/ai_tools.py
+++ decman/modules/common/ai_tools.py
@@ -1,11 +1,16 @@
 import decman
-from decman.plugins import aur
+from decman.plugins import aur, pacman
 
 
 class AIToolsModule(decman.Module):
     def __init__(self):
         super().__init__("ai_tools")
 
+    @pacman.packages
+    def pkgs(self) -> set[str]:
+        return {
+            "opencode",
+        }
     @aur.packages
     def aurpkgs(self) -> set[str]:
         return {
